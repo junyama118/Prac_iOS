ed5ec2e4de17c120b36ea9c5c4aa638cd2144a80
Jum Yamahira <jun.yamahira@gmo-media.jp>
2020-01-10 16:21:13 +0900

    Initial Commit

---
+++ pracswift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pracswift
+//
+//  Created by 山平潤 on 2020/01/10.
+//  Copyright © 2020 山平潤. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
